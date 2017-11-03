71e27ed03ad458f908ffb1f317fc7485251bfe07
William Fawcett <william.fawcett@cern.ch>
2017-11-03 16:31:40 +0100

    adding script to run config generator

---
+++ config-generator/generateConfigs.sh
@@ -0,0 +1,15 @@
+python configGenerator.py -l 1 -s 50 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 40 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 35 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 30 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 25 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 20 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 10 -o ../../tkLayout/run/
+
+python configGenerator.py -l 2 -s 50 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 40 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 35 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 30 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 25 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 20 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 10 -o ../../tkLayout/run/
