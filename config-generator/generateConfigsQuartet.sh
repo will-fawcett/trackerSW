fb8c61fe054c7319c5a6383421a7153ef2ca17d5
William Fawcett <william.fawcett@cern.ch>
2017-11-16 14:54:47 +0100

    adding quartet generator:

---
+++ config-generator/generateConfigsQuartet.sh
@@ -0,0 +1,39 @@
+python configGenerator.py -l 1 -s 50 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 40 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 35 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 30 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 25 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 20 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 1 -s 10 -q 1 -o ../../tkLayout/run/
+
+python configGenerator.py -l 2 -s 50 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 40 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 35 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 30 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 25 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 20 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 2 -s 10 -q 1 -o ../../tkLayout/run/
+
+python configGenerator.py -l 3 -s 50 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 3 -s 40 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 3 -s 35 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 3 -s 30 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 3 -s 25 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 3 -s 20 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 3 -s 10 -q 1 -o ../../tkLayout/run/
+
+python configGenerator.py -l 4 -s 50 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 4 -s 40 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 4 -s 35 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 4 -s 30 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 4 -s 25 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 4 -s 20 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 4 -s 10 -q 1 -o ../../tkLayout/run/
+
+python configGenerator.py -l 5 -s 50 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 5 -s 40 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 5 -s 35 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 5 -s 30 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 5 -s 25 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 5 -s 20 -q 1 -o ../../tkLayout/run/
+python configGenerator.py -l 5 -s 10 -q 1 -o ../../tkLayout/run/
