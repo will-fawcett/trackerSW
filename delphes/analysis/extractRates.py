5db60d46da56488de12e65e1f9416e0df5562221
William Fawcett <william.fawcett@cern.ch>
2017-12-06 23:40:37 +0100

    adding script to extract trigger rates

---
+++ delphes/analysis/extractRates.py
@@ -0,0 +1,73 @@
+from ROOT import * 
+gROOT.SetBatch(1)
+gROOT.SetBatch(1)
+gStyle.SetGridStyle(3) 
+gStyle.SetPadLeftMargin(0.15) # increase space for left margin
+gStyle.SetPadBottomMargin(0.15) # increase space for left margin
+gStyle.SetGridColor(kGray)
+gStyle.SetPadTickX(1) # add tics on top x
+gStyle.SetPadTickY(1) # add tics on right y
+
+def main(inputFile, outputDir):
+    outputDir = appendSlash(outputDir)
+
+    ifile = TFile.Open(inputFile)
+
+    # extract jet pT histogram
+    h = ifile.Get('jet4Pt')
+    c = getReverseCumulativeHisto(h)
+
+    
+    can = TCanvas('can', 'can', 500, 500)
+    h.GetXaxis().SetRangeUser(0, 200)
+    h.Draw()
+    can.SaveAs(outputDir+'testJetPt.pdf')
+
+    c.GetXaxis().SetRangeUser(0, 200)
+    c.SetMarkerColor(865)
+    c.SetLineColor(865)
+    c.GetYaxis().SetTitle('Relative Rate')
+
+    c.DrawNormalized()
+    can.SaveAs(outputDir+'testJetCumulativePt.pdf')
+
+
+def appendSlash(path):
+    if path[-1] != '/':
+        return path+'/'
+    else:
+        return path
+
+#____________________________________________________________________________
+def getReverseCumulativeHisto(histo):
+    '''
+    Function to return the cumulative efficiency curve from a
+    differential efficiency curve
+    '''
+    nbins = histo.GetNbinsX()
+    error = Double()
+    cumulHisto = histo.Clone()
+    cumulHisto.Reset()
+    for bin in range(1,nbins+1):
+        integral = histo.IntegralAndError(bin,nbins,error)
+        integralErr = error
+        cumulHisto.SetBinContent(bin,integral)
+        cumulHisto.SetBinError(bin,integralErr)
+    return cumulHisto
+    
+
+if __name__ == "__main__":
+
+    from optparse import OptionParser
+    import sys
+
+    parser = OptionParser()
+    parser.add_option("-i", "--inputFile", action="store", type="string", help="Input ROOT file")
+    parser.add_option("-o", "--outputDir", action="store", type="string",  default='./', help="Output directory for plots")
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(0)
+
+    options, args = parser.parse_args()
+    option_dict = dict( (k, v) for k, v in vars(options).iteritems() if v is not None)
+    main(**option_dict)
