63706965eb495780ae632c4d443518bb595a125f
William James Fawcett <william.fawcett@cern.ch>
2018-01-31 10:14:18 +0100

    Adding script to plot distribution of fake rates

---
+++ plotting/fakeRatePlotter.py
@@ -0,0 +1,62 @@
+from ROOT import *
+from colours import colours
+from extract_coords import prepareLegend
+
+
+gROOT.SetBatch(1)
+gStyle.SetPadBottomMargin(0.15)
+gStyle.SetPadTickX(1)
+gStyle.SetPadTickY(1)
+
+path = "/Users/Will/Desktop/hits/"
+
+
+def main():
+
+    can = TCanvas("can", "can", 500, 500)
+    for PILEUP in [1, 200]:
+    
+        # filesystem 
+        fName = path+"hits_ttbar_pu{0}_result.root".format(PILEUP) 
+        outputDir = "FakeRate/"
+        ifile = TFile.Open(fName)
+
+        # histograms
+        nDelphesTracks = ifile.Get("nDelphesTracks")
+        nDelphesTracks1GeV = ifile.Get("nDelphesTracks1GeV")
+        nRecoTracks = ifile.Get("nRecoTracks")
+
+        # stats
+        meanDelphesTracks = nDelphesTracks1GeV.GetMean()
+        stdDevDelphesTracks = nDelphesTracks.GetStdDev()
+
+        meanRecoTracks = nRecoTracks.GetMean()
+        stdDevDelphesTracks = nRecoTracks.GetStdDev()
+
+
+        # styling
+        REBIN = 4
+        nDelphesTracks1GeV.Draw()
+        xaxis = nDelphesTracks1GeV.GetXaxis()
+        nDelphesTracks1GeV.SetLineColor(colours.blue)
+        nDelphesTracks1GeV.Rebin(REBIN)
+
+        if PILEUP == 1:
+            xaxis.SetRangeUser(0, 500)
+
+        nRecoTracks.SetLineColor(colours.green)
+        nRecoTracks.Rebin(REBIN)
+        nRecoTracks.Draw("same")
+
+        # Legend
+        leg = prepareLegend("topRight")
+        leg.AddEntry(nDelphesTracks1GeV, "True", "l")     
+        leg.AddEntry(0, "mean : {0}".format(meanDelphesTracks), "")
+        leg.AddEntry(nRecoTracks, "Reco", "l")
+        leg.AddEntry(0, "mean : {0}".format(meanRecoTracks), "")
+        leg.Draw()
+
+        can.SaveAs(outputDir+"trackComparrison_pu{0}.pdf".format(PILEUP))
+
+if __name__ == "__main__":
+    main()
